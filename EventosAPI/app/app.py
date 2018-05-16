5eeef95a287fecabe59e85b75d1e42b1a0bb1aa1
Eduardo <jemedina.96@gmail.com>
2018-05-16 15:32:46 -0500

    EventosAPI Fix params

--- EventosAPI/app/app.py
+++ EventosAPI/app/app.py
@@ -59,11 +59,9 @@ def reserved_seats_by_section(seccion, folio, fecha, hora):
 
 def buildSeatsReponse(seat):
     return {
-        'num_asiento': seat[0],
-        'seccion': seat[1],
-        'folio': seat[2],
-        'fecha': int(time.mktime(seat[3].timetuple()))*1000,
-        'hora': seat[4]
+        'asientos': seat[1],
+        'titular': seat[2],
+        'seccion': seat[3]
     }
 
 @flaskapp.route('/funciones/asientos-reservados-por-titular/<no_tarjeta>')
@@ -75,9 +73,9 @@ def seats_by_titular(no_tarjeta):
         funciones.append(buildSeatsReponse(func))
     return jsonify(funciones)
 
-@flaskapp.route('/funciones/all-seats/<seccion>/<folio>/<fecha>/<hora>')
-def all_seats_by_section(seccion, folio, fecha, hora):
-    funcionesResult = executeQuery('''SELECT * FROM asiento WHERE seccion = {}'''.format(seccion),''' and folio = {}'''.format(folio),''' and fecha = {}'''.format(fecha),''' and hora = {}'''.format(hora))
+@flaskapp.route('/funciones/all-seats/<id_funcion>/<seccion>')
+def all_seats_by_section(id_funcion, seccion):
+    funcionesResult = executeQuery('''SELECT * FROM asiento WHERE id_funcion = {}'''.format(id_funcion)+''' and seccion = '{}' '''.format(seccion))
     print(funcionesResult)
     funciones = []
     for func in funcionesResult:
@@ -95,6 +93,31 @@ def events_data(folio):
         appendHorariosToFunciones(funciones, horario)
     print(funciones)
     return jsonify(funciones)
+
+@flaskapp.route('/funciones/preciosporevento/<folio>')
+def preciosAsientos(folio):
+    preciosResult = executeQuery('''SELECT * FROM precios_evento  WHERE folio_evento = {}'''.format(folio))
+    precios = []
+    for precio in preciosResult:
+        precios.append(buildPreciosResponse(precio))
+    return jsonify(precios)
+
+
+@flaskapp.route('/funciones/save/<funcion_id>/<folio_artista>/<seccion>/<asientos>/<cardNumber>/<cardCvc>')
+def guardarReservacion(funcion_id,folio_artista,seccion,asientos,cardNumber,cardCvc):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    try:
+        query = '''INSERT INTO asiento VALUES ({},"{}","{}","{}")'''.format(funcion_id,asientos,cardNumber,seccion)
+        res = cursor.execute(query)
+        conn.commit()
+        print('Query:',query)
+        return jsonify(True)
+    except Exception as e:
+        print("Error during insert:",str(e))
+        return jsonify(False)
+
+
 
 @flaskapp.route('/funciones/all')
 def all_events():
@@ -131,5 +154,12 @@ def buildEventsReponse(events):
             'mid': events[7],
             'low': events[8],					
 		}
+    }
+def buildPreciosResponse(precios):
+    return {
+        'folio_evento': precios[0],
+        'top': precios[1],
+        'mid': precios[2],
+        'low': precios[3]
     }
 
